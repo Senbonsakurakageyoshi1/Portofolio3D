268e1cf44bfb6c3da242c84d7196b102868757f3
Senbonsakurakageyoshi1 <699580621meliga@gmail.com>
2024-01-02 09:15:08 +0100

    build in build folder

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: 'build', // Spécifie le nom du dossier de sortie
+  },
 })
